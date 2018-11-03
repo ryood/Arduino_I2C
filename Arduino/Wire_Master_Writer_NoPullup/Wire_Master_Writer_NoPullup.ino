5b25e5a840a4b460891a4859c71f296377b53adf
DESKTOP-DKK6M9Q\nerdfloor <kawanabe@gmail.com>
2018-11-03 14:24:16 +0900

    first commit

---
+++ Arduino/Wire_Master_Writer_NoPullup/Wire_Master_Writer_NoPullup.ino
@@ -0,0 +1,32 @@
+// Wire Master Writer
+// by Nicholas Zambetti <http://www.zambetti.com>
+
+// Demonstrates use of the Wire library
+// Writes data to an I2C/TWI slave device
+// Refer to the "Wire Slave Receiver" example for use with this
+
+// Created 29 March 2006
+
+// This example code is in the public domain.
+
+
+#include <Wire.h>
+
+void setup() {
+  Wire.begin(); // join i2c bus (address optional for master)
+  pinMode(A4, INPUT);  // disable pullup
+  pinMode(A5, INPUT);  // disable pullup
+  Wire.setClock(400000);
+}
+
+byte x = 0;
+
+void loop() {
+  Wire.beginTransmission(8); // transmit to device #8
+  Wire.write("x is ");        // sends five bytes
+  Wire.write(x);              // sends one byte
+  Wire.endTransmission();    // stop transmitting
+
+  x++;
+  delay(500);
+}
